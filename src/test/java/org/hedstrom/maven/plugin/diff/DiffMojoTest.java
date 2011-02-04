b4e9425a36d998b605e25d3250162ed9d7cf4bf6
Anders Hedström <anders.hedstroem@gmail.com>
2011-02-04 13:48:36 +0100

    added test class for mojo

---
+++ src/test/java/org/hedstrom/maven/plugin/diff/DiffMojoTest.java
@@ -0,0 +1,13 @@
+package org.hedstrom.maven.plugin.diff;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class DiffMojoTest {
+
+	@Test
+	public void test() {
+		Assert.assertTrue(true);
+	}
+}
